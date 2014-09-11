9ee9b5a0a8e43fb43842ef307ec9b5ead1976e77
Kuangyu Wang <wangw1023@gmail.com>
2014-09-11 13:07:56 -0400

    stepByStep test

--- src/test/beast/evolution/likelihood/YN98Fast_stepByStep.java
+++ src/test/beast/evolution/likelihood/YN98Fast_stepByStep.java
@@ -14,7 +14,7 @@ import junit.framework.TestCase;
 public class YN98Fast_stepByStep extends TestCase {
 
 		@Test
-		public void testRateMatrix() throws Exception {			
+		public void testRateMatrix_allEqual() throws Exception {			
 	        RealParameter f = new RealParameter(new Double[]{0.25,0.25,0.25,0.25});
 	        Frequencies nucleoFrequencies = new Frequencies();
 	        nucleoFrequencies.initByName("frequencies", f, "estimate", false);
@@ -39,4 +39,32 @@ public class YN98Fast_stepByStep extends TestCase {
 	        //System.setOut(new PrintStream(new FileOutputStream("/Users/kwang2/Desktop/YN98_freq.txt")));
 	        System.out.println(Arrays.toString(yn98.getFrequencies()));
 		}
+		
+		@Test
+		public void testRateMatrix_allDiff() throws Exception {			
+	        RealParameter f = new RealParameter(new Double[]{0.308,0.228,0.23,0.234});
+	        Frequencies nucleoFrequencies = new Frequencies();
+	        nucleoFrequencies.initByName("frequencies", f, "estimate", false);
+	        YN98Fast yn98 = new YN98Fast();
+	        yn98.initByName("kappa", "1", "omega", "1.0", "nucleoFrequencies", nucleoFrequencies);
+	        
+	        yn98.prepareMatricesForTest();
+
+	        System.setOut(new PrintStream(new FileOutputStream("/home/kuangyu/Desktop/YN98_symmM_updated.txt")));
+	        //System.setOut(new PrintStream(new FileOutputStream("/Users/kwang2/Desktop/YN98_symmM.txt")));
+	        System.out.println(Arrays.deepToString(yn98.getSymmMatrix()));
+	        
+	        System.setOut(new PrintStream(new FileOutputStream("/home/kuangyu/Desktop/YN98_diagM_updated.txt")));
+	        //System.setOut(new PrintStream(new FileOutputStream("/Users/kwang2/Desktop/YN98_diagM.txt")));
+	        System.out.println(Arrays.toString(yn98.getDiagMatrix()));
+	        
+	        System.setOut(new PrintStream(new FileOutputStream("/home/kuangyu/Desktop/YN98_rateM_updated.txt")));
+	        //System.setOut(new PrintStream(new FileOutputStream("/Users/kwang2/Desktop/YN98_rateM.txt")));
+	        System.out.println(Arrays.deepToString(yn98.getRateMatrix()));
+	        
+	        System.setOut(new PrintStream(new FileOutputStream("/home/kuangyu/Desktop/YN98_freq_updated.txt")));
+	        //System.setOut(new PrintStream(new FileOutputStream("/Users/kwang2/Desktop/YN98_freq.txt")));
+	        System.out.println(Arrays.toString(yn98.getFrequencies()));
+		}
+		
 }

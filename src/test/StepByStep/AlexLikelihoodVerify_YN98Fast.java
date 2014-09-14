ac866f3ac7c565f8b7cb23afec7b1d2cfcf192f9
Kuangyu Wang <wangw1023@gmail.com>
2014-09-14 15:21:24 -0400

    test using Alex TreeLikelihood

--- src/test/StepByStep/AlexLikelihoodVerify_YN98Fast.java
+++ src/test/StepByStep/AlexLikelihoodVerify_YN98Fast.java
@@ -21,8 +21,8 @@ import junit.framework.TestCase;
 public class AlexLikelihoodVerify_YN98Fast extends TestCase {
 
     static public Alignment getData_1site() throws Exception {
-        Sequence seqA = new Sequence("A", "AAA");
-        Sequence seqB = new Sequence("B", "TTT");
+        Sequence seqA = new Sequence("A", "AAATGTAAA");
+        Sequence seqB = new Sequence("B", "TTTGGGTTT");
 
         UserDataType codon = new UserDataType();
         codon.initByName("states", 61, "codelength", 3, "codeMap", "AAA=0, AAC=1, AAG=2, AAT=3, ACA=4, ACC=5, ACG=6, ACT=7, AGA=8, AGC=9, AGG=10, AGT=11, ATA=12, ATC=13, ATG=14, ATT=15, CAA=16, CAC=17, CAG=18, CAT=19, CCA=20, CCC=21, CCG=22, CCT=23, CGA=24, CGC=25, CGG=26, CGT=27, CTA=28, CTC=29, CTG=30, CTT=31, GAA=32, GAC=33, GAG=34, GAT=35, GCA=36, GCC=37, GCG=38, GCT=39, GGA=40, GGC=41, GGG=42, GGT=43, GTA=44, GTC=45, GTG=46, GTT=47, TAC=48, TAT=49, TCA=50, TCC=51, TCG=52, TCT=53, TGC=54, TGG=55, TGT=56, TTA=57, TTC=58, TTG=59, TTT=60");
